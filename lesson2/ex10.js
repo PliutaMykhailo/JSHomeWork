2b55a5d6914c2d08e3aac91e9ade5a85b34db489
a <mykhailo.pliuta@gmail.com>
2022-01-14 08:48:47 +0200

    add ex10

---
+++ lesson2/ex10.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Отсортировать массив по возрастанию.
+// [1,2,3,4,5,6]
+
+var arr = [6, 5, 4, 3, 2, 1];
+var arrNew = [];
+var i;
+var j;
+var l = arr.length - 1;
+
+for (j = 0; j < arr.length; j++) {
+  for (i = 0; i < l; i++) {
+    if (arr[i] < arr[i + 1]) {
+      var min = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i] = min;
+    }
+  }
+
+  arrNew[j] = arr[i];
+  l = l - 1;
+}
+
+console.log(arrNew);
