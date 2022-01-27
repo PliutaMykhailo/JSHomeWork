403302159c84f8ff4bd6427d9326d4a07d115b48
a <mykhailo.pliuta@gmail.com>
2022-01-27 14:14:26 +0200

    add ex4

---
+++ lesson4/ex1.js
@@ -0,0 +1,31 @@
+/*
+Создать имплементацию функции `forEach`, логика работы такая же как и у родного метода.
+
+Функция должна содержать проверки:
+
+- Первый параметр обязателен и может принимать только массив
+- Второй параметр обязателен и может принимать только функцию
+
+const arr = [1,2,3];
+forEach(arr, function(item, i, arr) {});
+*/
+
+function myForEach(arr, callback) {
+  if (Array.isArray(arr)) {
+    if (typeof callback === "function") {
+      for (var i = 0; i < arr.length; i = i + 1) {
+        callback(arr[i], i, arr);
+      }
+    } else {
+      throw new Error("Error: parameter type is not a function");
+    }
+  } else {
+    throw new Error("Error: parameter type is not a array");
+  }
+}
+
+const arr = [1, 2, 3];
+
+myForEach(arr, function (item, i, arr) {
+  console.log("number: " + item + ", index: " + i + ", array:", arr);
+});
