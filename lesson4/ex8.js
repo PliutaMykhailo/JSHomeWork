7520bc84837f1bf3f0d9012b67959ad6d939659d
a <mykhailo.pliuta@gmail.com>
2022-01-28 18:55:50 +0200

    add ex8

---
+++ lesson4/ex8.js
@@ -0,0 +1,63 @@
+/*
+Напишите функцию `f` . Данная функция принимает один параметр: одноуровневый или многоуровневый массив. Возвращает данная функция сумму всех элементов на всех уровнях.
+
+Обратите внимание что функция должна возвращать число 0, если при проходе всех уровней не было найдено ни одного числа.
+
+Функция должна содержать проверки:
+
+- Первый параметр обязателен и может принимать только массив
+- Генерировать ошибку если на каком то уровне было найдено не число и не массив
+
+```javascript
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+f(arr); // 12
+const arr2 = [[[[[1,2]]]]];
+f(arr2); // 3
+const arr3 = [[[[[1,2]]],2],1];
+f(arr3); // 6
+const arr4 = [[[[[]]]]];
+f(arr4); // 0
+const arr5 = [[[[[],3]]]];
+f(arr5); // 3
+*/
+
+function f(arr) {
+  if (Array.isArray(arr)) {
+    var sum = 0;
+    var arrNew = arr.flat(Infinity);
+    for (var i = 0; i < arrNew.length; i = i + 1) {
+      if (typeof arrNew[i] === "number" || typeof arrNew[i] === "object") {
+        sum = sum + arrNew[i];
+      } else {
+        throw new Error("Error: value type is not a number or an object");
+      }
+    }
+    console.log(sum);
+  } else {
+    throw new Error("Error: parameter type is not a array");
+  }
+}
+
+const arr = [
+  [
+    [1, 2],
+    [1, 2],
+  ],
+  [
+    [2, 1],
+    [1, 2],
+  ],
+];
+f(arr); // 12
+
+const arr2 = [[[[[1, 2]]]]];
+f(arr2); // 3
+
+const arr3 = [[[[[1, 2]]], 2], 1];
+f(arr3); // 6
+
+const arr4 = [[[[[]]]]];
+f(arr4); // 0
+
+const arr5 = [[[[[], 3]]]];
+f(arr5); // 3
