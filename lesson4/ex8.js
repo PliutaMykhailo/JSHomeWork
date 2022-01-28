2a6e9f7b7be3ec8fefab45f3bb6582518d323333
a <mykhailo.pliuta@gmail.com>
2022-01-28 19:32:09 +0200

    add ex8

--- lesson4/ex8.js
+++ lesson4/ex8.js
@@ -32,7 +32,7 @@ function f(arr) {
         throw new Error("Error: value type is not a number or an object");
       }
     }
-    console.log(sum);
+    return sum;
   } else {
     throw new Error("Error: parameter type is not a array");
   }
@@ -48,16 +48,16 @@ const arr = [
     [1, 2],
   ],
 ];
-f(arr); // 12
+console.log(f(arr)); // 12
 
 const arr2 = [[[[[1, 2]]]]];
-f(arr2); // 3
+console.log(f(arr2)); // 3
 
 const arr3 = [[[[[1, 2]]], 2], 1];
-f(arr3); // 6
+console.log(f(arr3)); // 6
 
 const arr4 = [[[[[]]]]];
-f(arr4); // 0
+console.log(f(arr4)); // 0
 
 const arr5 = [[[[[], 3]]]];
-f(arr5); // 3
+console.log(f(arr5)); // 3
